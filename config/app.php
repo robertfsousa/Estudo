6f27589c7e14f8ef028abc2ed2032bdb2ba9abbb
Robert <robertfsousa@yahoo.com.br>
2016-09-20 12:38:02 -0300

    Novo

--- config/app.php
+++ config/app.php
@@ -177,8 +177,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Jlapp\Swaggervel\SwaggervelServiceProvider::class,
-
+        Laravel\Passport\PassportServiceProvider::class,
 
     ],
 
